dcb5432d4425da5e1c1c7d1ef8e544d97f708709
benyAlves <malulequeb@gmail.com>
2020-06-15 15:05:34 +0200

    refactor: add dagger scopes to reuse user manager instance

--- app/src/main/java/com/example/android/dagger/di/StorageModule.kt
+++ app/src/main/java/com/example/android/dagger/di/StorageModule.kt
@@ -1,4 +1,13 @@
 package com.example.android.dagger.di
 
-class StorageModule {
+import com.example.android.dagger.storage.SharedPreferencesStorage
+import com.example.android.dagger.storage.Storage
+import dagger.Binds
+import dagger.Module
+
+@Module
+abstract class StorageModule {
+
+    @Binds
+    abstract fun provideStorage(storage: SharedPreferencesStorage): Storage
 }
